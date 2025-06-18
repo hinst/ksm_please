41841dc9f5f70b1fd2ed8c2c76ff374b0572cba7
hinst <hinstance@yandex.ru>
2025-06-18 13:05:18 +0200

    Read all services from systemctl

--- main.go
+++ main.go
@@ -3,16 +3,23 @@ package main
 import (
 	"log"
 	"os/exec"
+	"regexp"
 	"strings"
 )
 
 func getAllServices() []string {
+	var matcher = regexp.MustCompile(`\s[\S-]+`)
 	var allUnitsText = string(assertResultError(exec.Command("systemctl", "list-units", "--type=service", "--all").Output()))
 	for index, unitText := range strings.Split(allUnitsText, "\n") {
-		if 0 == index {
+		if len(strings.TrimSpace(unitText)) == 0 {
+			break
+		}
+		if index == 0 {
 			continue
 		}
-		log.Println(unitText)
+		var unitName = matcher.FindString(unitText)
+		unitName = strings.TrimSpace(unitName)
+		log.Println(unitName)
 	}
 	return nil
 }
